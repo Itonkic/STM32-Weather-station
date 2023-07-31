79327e26e8d8ac5b203f09a10a85b1a7b6ed1a61
Itonkic <ivan.tonkic@gmail.com>
2023-07-31 09:12:08 +0200

    Update Main.ino
    added all remaining things from checklist

--- STM32/Main.ino
+++ STM32/Main.ino
@@ -1,6 +1,7 @@
 #include <BH1750.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <SD.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 #include <LiquidCrystal_I2C.h>
@@ -13,10 +14,46 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 Adafruit_VEML6070 uv = Adafruit_VEML6070();
 #define VEML6070_ADDR_L (0x39) ///< Low address
 MechaQMC5883 qmc;
+unsigned long currentMilliss;
+unsigned long previousMilliss = 0;
+float lux;
+float stupnjevi;
+float tlak;
+float vlaga;
+int uvv;
+float kompas;
+File file;
+int lastState = HIGH;
+int bLUX = PA10;
+int btemp = PA9;
+int btlak = PA8;
+int bvlaga = PB15;
+int buv = PB14;
+int bkompas = PB13;
+
+float readcompass() {
+  int x,y,z;
+  qmc.read(&x,&y,&z);
+  String xx=String(x);
+  String yy=String(y);
+  String zz=String(z);
+  String asd="x"+xx+" y"+yy+" z"+zz;
+  float heading = atan2(y, x);
+  float declinationAngle = 0.0878;
+  heading += declinationAngle;
+  if(heading < 0)
+    heading += 2*PI;
+  if(heading > 2*PI)
+    heading -= 2*PI;
+  float headingDegrees = heading * 180/M_PI; 
+  return headingDegrees;
+  }
+  
 void setup() {
   
     Serial.begin(9600);
     Wire.begin();
+    qmc.init();
     lcd.begin();
     uv.begin(VEML6070_1_T);
     lcd.backlight();
@@ -32,7 +69,7 @@ void setup() {
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
-  lcd.clear();
+ // lcd.clear();
 }
 
 void loop() {
@@ -43,78 +80,83 @@ void loop() {
     Serial.println(F("Failed to begin reading :("));
     return;
   }
-
+  if (!SD.begin()) {
+    Serial.println("begin failed");
+    return;
+  }
   delay(50); // This represents parallel work.
-
   if (!bme.endReading()) {
     Serial.println(F("Failed to complete reading :("));
     return;
   }
-  float lux = lightMeter.readLightLevel();
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("LUX ");
-  lcd.setCursor(0,1);
-  lcd.print(lux);
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("temp ");
-  lcd.setCursor(0,1);
-  lcd.print(bme.temperature);
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(("Pressure hPa = "));
-  lcd.setCursor(0,1);
-  lcd.print(bme.pressure / 100.0);
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(("Humidity % = "));
-  lcd.setCursor(0,1);
-  lcd.print(bme.humidity);
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(("UV Index"));
-  lcd.setCursor(0,1);
-  lcd.print((uv.readUV()/230));
-  delay(1000);
-  int x,y,z;
-  qmc.read(&x,&y,&z);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(("compas"));
-  lcd.setCursor(0,1);/*
-  String xx=String(x);
-  String yy=String(y);
-  String zz=String(z);
-  String asd="x"+xx+" y"+yy+" z"+zz;*/
-  float heading = atan2(y, x);
-  float declinationAngle = 0.0878;
-  heading += declinationAngle;
-  if(heading < 0)
-    heading += 2*PI;
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
-  float headingDegrees = heading * 180/M_PI; 
-
-  lcd.print(headingDegrees);
-  delay(1000);
-
-
-
-
-
-
-
-
-
-
-
-
+  currentMilliss = millis();
+  unsigned long intervall = 5000;
+  if (currentMilliss - previousMilliss > intervall) {
+    lux = lightMeter.readLightLevel();
+    stupnjevi = bme.temperature;
+    tlak = (bme.pressure / 100.0);
+    vlaga = bme.humidity;
+    uvv = (uv.readUV()/230);
+    kompas = readcompass();
+    file = SD.open("data.TXT", FILE_WRITE);
+    file.println("Temperatura "+String(stupnjevi));
+    file.println("Lux "+String(lux));
+    file.println("Tlak "+String(tlak));
+    file.println("Vlaga "+String(vlaga));
+    file.println("UV "+String(uvv));
+    file.println("Kompas "+String(kompas));
+    file.close();
+    previousMilliss = currentMilliss;
+  }
+  
+  int buttonstate =digitalRead(bLUX);
+  if (lastState==HIGH && buttonstate == LOW){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("LUX ");
+      lcd.setCursor(0,1);
+      lcd.print(lux);
+    }
+  int buttonstate1 =digitalRead(btemp);
+  if (lastState==HIGH && buttonstate1 == LOW){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("temp ");
+      lcd.setCursor(0,1);
+      lcd.print(stupnjevi);
+    }
+  int buttonstate2 =digitalRead(btlak);
+  if (lastState==HIGH && buttonstate2 == LOW){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Pressure hPa =  ");
+      lcd.setCursor(0,1);
+      lcd.print(tlak);
+    }
+    
+  int buttonstate3 =digitalRead(bvlaga);
+  if (lastState==HIGH && buttonstate3 == LOW){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Humidity % = ");
+      lcd.setCursor(0,1);
+      lcd.print(vlaga);
+    }
+      int buttonstate4 =digitalRead(buv);
+  if (lastState==HIGH && buttonstate4 == LOW){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("UV index ");
+      lcd.setCursor(0,1);
+      lcd.print(uvv);
+    }
+      int buttonstate5 =digitalRead(bkompas);
+  if (lastState==HIGH && buttonstate5 == LOW){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("compas ");
+      lcd.setCursor(0,1);
+      lcd.print(readcompass());
+    }
   
 }
